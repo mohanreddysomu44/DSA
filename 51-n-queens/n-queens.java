9dbb290e4b27efa61ac2472f70ca27f0a128c442
Somu Mohan Reddy <mohanreddysomu44@gmail.com>
2026-01-19 11:44:40 +0530

    Time: 2 ms (79.47%) | Memory: 46.7 MB (52.25%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,51 @@
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        backtrack(board, 0, n);
+        return ans;
+    }
+
+    private void backtrack(char[][] board, int row, int n) {
+        if (row == n) {
+            ans.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1, n);
+                board[row][col] = '.'; // backtrack
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+        // check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        // check diagonal left-up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        // check diagonal right-up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            res.add(new String(board[i]));
+        }
+        return res;
+    }
+}
